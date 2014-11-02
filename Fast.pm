eb0bcaa81b339bb67ecaec9d6c814f9519336f33
Michal Špaček <tupinek@gmail.com>
2014-11-02 12:23:31 +0100

    Add EXAMPLE section to doc.

--- Fast.pm
+++ Fast.pm
@@ -83,6 +83,47 @@ CGI::Pure::Fast - Fast Common Gateway Interface Class for CGI::Pure.
 
  Other methods are same as CGI::Pure.
 
+=head1 EXAMPLE
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use CGI::Pure::Fast;
+ use HTTP::Headers;
+
+ # HTTP header.
+ my $header = HTTP::Headers->new;
+ $header->header('Content-Type' => 'text/html');
+
+ # FCGI script.
+ my $count = 1;
+ while (my $cgi = CGI::Pure::Fast->new) {
+         print $header->as_string."\n";
+         print $count++."\n";
+ }
+
+ # Output in CGI mode:
+ # Content-Type: text/html
+ # 
+ # 1
+ # ...
+ # Content-Type: text/html
+ # 
+ # 1
+ # ...
+
+ # Output in FASTCGI mode:
+ # Content-Type: text/html
+ # 
+ # 1
+ # ...
+ # Content-Type: text/html
+ # 
+ # 2
+ # ...
+
 =head1 DEPENDENCIES
 
 L<CGI::Pure>,
